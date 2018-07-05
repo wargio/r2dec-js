676731e952d2e8c7813012a6256192ca54e04cc2
elicn <elichn@gmail.com>
2018-07-06 00:58:25 +0300

    Function calls now display their arguments correctly on x86

--- libdec/core/Analyzer.js
+++ libdec/core/Analyzer.js
@@ -23,7 +23,16 @@ module.exports = (function() {
     var Instruction = require('libdec/core/Instruction');
     var XRefs = require('libdec/core/XRefs');
     var Strings = require('libdec/core/Strings');
+    var Functions = require('libdec/core/Functions');
     var Routine = require('libdec/core/Routine');
+
+    var _handle_functions = function(functions, instr, options) {
+        instr.forEach(function(e) {
+            if (e.type == 'call') { // 'jmp', 'ujmp' ?
+                e.callee = functions.search(e.jump);
+            }
+        });
+    };
 
     var _resolve_xref = function(xrefs, instr, options) {
         instr.forEach(function(e) {
@@ -110,6 +119,11 @@ module.exports = (function() {
             var instructions = routine.instructions;
             var strings = new Strings(izj);
             _resolve_strings(strings, instructions, this.options);
+        };
+        this.functions = function(routine, aflj) {
+            var instructions = routine.instructions;
+            var functions = new Functions(aflj);
+            _handle_functions(functions, instructions, this.options);
         };
         this.analyze = function(routine, arch) {
             var context = arch.context();
