e3c50fff9ea00cd2c872cb1117d69d45d1dfcd95
Giovanni <wargio@libero.it>
2018-07-12 17:26:23 +0200

    Update ppc.js

--- libdec/arch/ppc.js
+++ libdec/arch/ppc.js
@@ -541,7 +541,7 @@ module.exports = (function() {
             } else {
                 var value = Base.variable();
                 ops.push(Base.instructions.rotate_left(value, src, sh, 32));
-                ops.push(Base.instructions.and(instr.parsed[1], value, m.toString(16)));
+                ops.push(Base.instructions.and(dst, value, m.toString(16)));
             }
         } else {
             var value0 = Base.variable();
