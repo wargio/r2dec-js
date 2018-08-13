4f03904a3f3d4f655ad71ca5ab4a70d6ff9c949a
wargio <wargio@libero.it>
2018-08-13 14:54:44 +0200

    if branch do not exists, switch to master

--- travis-test.sh
+++ travis-test.sh
@@ -9,7 +9,8 @@ fi
 cd ..
 WORKINGDIR_TRAVIS=$(pwd)
 ls r2dec-js >/dev/null 2>&1 || git clone --depth 1 https://github.com/wargio/r2dec-js
-ls r2dec-regression >/dev/null 2>&1 || git clone --branch "$BRANCH" --depth 1 https://github.com/wargio/r2dec-regression
+rm -rf r2dec-regression >/dev/null 2>&1 || echo "no need to clean.."
+git clone --branch "$BRANCH" --depth 1 https://github.com/wargio/r2dec-regression || git clone --depth 1 https://github.com/wargio/r2dec-regression
 cd r2dec-regression
 chmod +x testall.sh
 ./testall.sh "$WORKINGDIR_TRAVIS/r2dec-js" travis
