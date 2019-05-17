1fd3dd72b1ac69c24ba42b792810dc87e48bdcac
Giovanni <561184+wargio@users.noreply.github.com>
2019-05-17 13:53:20 +0200

    added slow and fast mode, to get values on demand (#160)

--- libdec/core/symbols.js
+++ libdec/core/symbols.js
@@ -16,6 +16,8 @@
  */
 
 module.exports = (function() {
+    var Long = require('libdec/long');
+    var r2pipe = require('libdec/r2pipe');
     var Utils = require('libdec/core/utils');
 
     var _compare = function(a, b) {
@@ -43,7 +45,7 @@ module.exports = (function() {
      * Expects the isj json as input.
      */
     return function(isj) {
-        this.data = isj.filter(_sanitize).sort(Global.evars.honor.paddr ? _physical_compare: _virtual_compare).map(function(x) {
+        this.data = isj.filter(_sanitize).sort(Global.evars.honor.paddr ? _physical_compare : _virtual_compare).map(function(x) {
             return {
                 location: Global.evars.honor.paddr ? x.paddr : x.vaddr,
                 value: x.demname.length > 0 ? x.demname : x.name,
@@ -51,6 +53,14 @@ module.exports = (function() {
         });
         this.search = function(address) {
             if (address) {
+                if (!Global.evars.extra.slow) {
+                    var def = {
+                        symbols: {}
+                    };
+                    var x = r2pipe.json64('is.j @ 0x' + address.toString(16), def).symbols;
+                    var loc = (Global.evars.honor.paddr ? x.paddr : x.vaddr) || Long.MAX_UNSIGNED_VALUE;
+                    return address.eq(loc) && !Long.MAX_UNSIGNED_VALUE.eq(loc) ? (x.demname.length > 0 ? x.demname : x.name) : null;
+                }
                 var r = Utils.search(address, this.data, _compare);
                 return r ? r.value : null;
             }
