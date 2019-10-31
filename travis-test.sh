d30dcc92bb89a641623bcfdc989a67ef3eb7e596
Giovanni <561184+wargio@users.noreply.github.com>
2019-10-31 10:38:18 +0100

    Transferred regressions

--- travis-test.sh
+++ travis-test.sh
@@ -2,7 +2,7 @@
 echo "Branch: $TRAVIS_BRANCH"
 make --no-print-directory testbin -C p
 ERRORED=$?
-if [[ "$ERRORED" == "1" ]]; then
+if [ "$ERRORED" == "1" ]; then
 	exit $ERRORED
 fi
 
@@ -11,7 +11,7 @@ cd ..
 WORKINGDIR_TRAVIS=$(pwd)
 ls r2dec-js >/dev/null 2>&1 || git clone --depth 1 https://github.com/radareorg/r2dec-js
 rm -rf r2dec-regression >/dev/null 2>&1 || echo "no need to clean.."
-git clone --branch "$TRAVIS_BRANCH" --depth 1 https://github.com/wargio/r2dec-regression || git clone --depth 1 https://github.com/wargio/r2dec-regression
+git clone --branch "$TRAVIS_BRANCH" --depth 1 https://github.com/radareorg/r2dec-regression || git clone --depth 1 https://github.com/radareorg/r2dec-regression
 cd r2dec-regression
 chmod +x testall.sh
 ./testall.sh "$WORKINGDIR_TRAVIS/r2dec-js" travis
