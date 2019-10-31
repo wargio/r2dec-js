f2e46ac7372fff31afe0220c341041329dd60fe3
Eli <elicn@users.noreply.github.com>
2019-10-31 09:46:00 +0200

    Update travis-test.sh per migration to radareorg

--- travis-test.sh
+++ travis-test.sh
@@ -2,14 +2,14 @@
 echo "Branch: $TRAVIS_BRANCH"
 make --no-print-directory testbin -C p
 ERRORED=$?
-if [[  "$ERRORED" == "1" ]]; then
+if [[ "$ERRORED" == "1" ]]; then
 	exit $ERRORED
 fi
 
 ## r2dec-regression
 cd ..
 WORKINGDIR_TRAVIS=$(pwd)
-ls r2dec-js >/dev/null 2>&1 || git clone --depth 1 https://github.com/wargio/r2dec-js
+ls r2dec-js >/dev/null 2>&1 || git clone --depth 1 https://github.com/radareorg/r2dec-js
 rm -rf r2dec-regression >/dev/null 2>&1 || echo "no need to clean.."
 git clone --branch "$TRAVIS_BRANCH" --depth 1 https://github.com/wargio/r2dec-regression || git clone --depth 1 https://github.com/wargio/r2dec-regression
 cd r2dec-regression
@@ -22,8 +22,6 @@ cd ..
 npm install -s eslint
 
 ## NPM test
-cd r2dec-js
-find ./libdec -type f -name "*.js" | xargs ../node_modules/.bin/eslint || ERRORED=1
-ls ./*.js | xargs ../node_modules/.bin/eslint || ERRORED=1
+find r2dec-js/ -type f -name "*.js" | xargs node_modules/.bin/eslint || ERRORED=1
 
 exit $ERRORED
