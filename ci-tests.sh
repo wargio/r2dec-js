4fe5084285e954b7fca8a3bafad38b1a8f18f3f7
wargio <wargio@libero.it>
2020-08-03 15:59:01 +0200

    moving to GH actions

--- ci-tests.sh
+++ ci-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Branch: $TRAVIS_BRANCH"
+CI_BRANCH="$1"
+echo "Branch: $CI_BRANCH"
 make --no-print-directory testbin -C p
 ERRORED=$?
 if [ "$ERRORED" == "1" ]; then
@@ -8,13 +9,13 @@ fi
 
 ## r2dec-regression
 cd ..
-WORKINGDIR_TRAVIS=$(pwd)
+WORKINGDIR_CI=$(pwd)
 ls r2dec-js >/dev/null 2>&1 || git clone --depth 1 https://github.com/radareorg/r2dec-js
 rm -rf r2dec-regression >/dev/null 2>&1 || echo "no need to clean.."
-git clone --branch "$TRAVIS_BRANCH" --depth 1 https://github.com/radareorg/r2dec-regression || git clone --depth 1 https://github.com/radareorg/r2dec-regression
+git clone --branch "$CI_BRANCH" --depth 1 https://github.com/radareorg/r2dec-regression || git clone --depth 1 https://github.com/radareorg/r2dec-regression
 cd r2dec-regression
 chmod +x testall.sh
-./testall.sh "$WORKINGDIR_TRAVIS/r2dec-js" travis
+./testall.sh "$WORKINGDIR_CI/r2dec-js" travis
 ERRORED=$?
 cd ..
 
