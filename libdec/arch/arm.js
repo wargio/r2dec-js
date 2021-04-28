cb677f5b871c3b175792cfeb5559729cbb58bd9f
Giovanni <561184+wargio@users.noreply.github.com>
2021-04-28 23:17:26 +0200

    Improved mips output & license update

--- libdec/arch/arm.js
+++ libdec/arch/arm.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2017-2018 deroad
+ * Copyright (C) 2017-2021 deroad
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- libdec/arch/x86.js
+++ libdec/arch/x86.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2018 deroad, elicn
+ * Copyright (C) 2017-2021 deroad, elicn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
