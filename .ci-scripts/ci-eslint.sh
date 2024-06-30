ff701173a7679062ea9fe9177a47979ec01436f1
wargio <wargio@libero.it>
2024-06-30 11:37:56 +0800

    Use node 21

--- .ci-scripts/ci-eslint.sh
+++ .ci-scripts/ci-eslint.sh
@@ -2,8 +2,7 @@
 set -e
 
 ## NPM eslint
-mkdir node_modules
 npm install --save-dev eslint
 
 ## NPM test
-find ./js -type f -name "*.js" | xargs node_modules/.bin/eslint -c .eslint.config.js
+find ./js -type f -name "*.js" | xargs node_modules/.bin/eslint --stats -c ./.eslint.config.js
