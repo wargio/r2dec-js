a8d8ca1dca5cf2b0703386204a49355fec0621ee
pancake <pancake@nopcode.org>
2026-04-09 22:37:35 +0200

    Improve nds32 support

--- js/libdec/arch/nds32.js
+++ js/libdec/arch/nds32.js
@@ -26,18 +26,20 @@ var _store = function(instr, bits, signed) {
 
 var _load_bi = function(instr, bits, signed) {
 	var e = instr.parsed;
+	var inc = e.opd.slice(2).join(' ') || '0';
 	var ops = [
 		Base.read_memory(e.opd[1], e.opd[0], bits, signed),
-		Base.increase(e.opd[1], e.opd[2] || '0')
+		Base.increase(e.opd[1], inc)
 	];
 	return Base.composed(ops);
 };
 
 var _store_bi = function(instr, bits, signed) {
 	var e = instr.parsed;
+	var inc = e.opd.slice(2).join(' ') || '0';
 	var ops = [
 		Base.write_memory(e.opd[1], e.opd[0], bits, signed),
-		Base.increase(e.opd[1], e.opd[2] || '0')
+		Base.increase(e.opd[1], inc)
 	];
 	return Base.composed(ops);
 };
@@ -115,6 +117,14 @@ export default {
 			var e = instr.parsed;
 			return Base.add(e.opd[0], e.opd[1], '(' + e.opd[2] + ' >> ' + e.opd[3] + ')');
 		},
+		sub_slli: function(instr) {
+			var e = instr.parsed;
+			return Base.subtract(e.opd[0], e.opd[1], '(' + e.opd[2] + ' << ' + e.opd[3] + ')');
+		},
+		sub_srli: function(instr) {
+			var e = instr.parsed;
+			return Base.subtract(e.opd[0], e.opd[1], '(' + e.opd[2] + ' >> ' + e.opd[3] + ')');
+		},
 		sub: function(instr) {
 			return _math3(instr, Base.subtract);
 		},
@@ -164,6 +174,21 @@ export default {
 			}
 			return Base.divide(e.opd[0], e.opd[1], e.opd[2]);
 		},
+		min: function(instr) {
+			var e = instr.parsed;
+			return Base.assign(e.opd[0], 'MIN(' + e.opd[1] + ', ' + e.opd[2] + ')');
+		},
+		max: function(instr) {
+			var e = instr.parsed;
+			return Base.assign(e.opd[0], 'MAX(' + e.opd[1] + ', ' + e.opd[2] + ')');
+		},
+		mulr64: function(instr) {
+			return _math3(instr, Base.multiply);
+		},
+		clips: function(instr) {
+			var e = instr.parsed;
+			return Base.assign(e.opd[0], 'CLIPS(' + e.opd[1] + ', ' + e.opd[2] + ')');
+		},
 		/* LOGIC */
 		and: function(instr) {
 			return _math3(instr, Base.and);
@@ -210,6 +235,26 @@ export default {
 		or_slli: function(instr) {
 			var e = instr.parsed;
 			return Base.or(e.opd[0], e.opd[1], '(' + e.opd[2] + ' << ' + e.opd[3] + ')');
+		},
+		or_srli: function(instr) {
+			var e = instr.parsed;
+			return Base.or(e.opd[0], e.opd[1], '(' + e.opd[2] + ' >> ' + e.opd[3] + ')');
+		},
+		xor_slli: function(instr) {
+			var e = instr.parsed;
+			return Base.xor(e.opd[0], e.opd[1], '(' + e.opd[2] + ' << ' + e.opd[3] + ')');
+		},
+		xor_srli: function(instr) {
+			var e = instr.parsed;
+			return Base.xor(e.opd[0], e.opd[1], '(' + e.opd[2] + ' >> ' + e.opd[3] + ')');
+		},
+		and_slli: function(instr) {
+			var e = instr.parsed;
+			return Base.and(e.opd[0], e.opd[1], '(' + e.opd[2] + ' << ' + e.opd[3] + ')');
+		},
+		and_srli: function(instr) {
+			var e = instr.parsed;
+			return Base.and(e.opd[0], e.opd[1], '(' + e.opd[2] + ' >> ' + e.opd[3] + ')');
 		},
 		/* SHIFTS */
 		slli: function(instr) {
@@ -348,10 +393,10 @@ export default {
 			return _load(instr, 32, false);
 		},
 		'lwi37.sp': function(instr) {
-			return _load(instr, 32, false);
+			return Base.read_memory('$sp + ' + _arg(instr, 1), _arg(instr, 0), 32, false);
 		},
 		'lwi45.fe': function(instr) {
-			return _load(instr, 32, false);
+			return Base.read_memory('$fp + ' + _arg(instr, 1), _arg(instr, 0), 32, false);
 		},
 		'lwi.gp': function(instr) {
 			return Base.read_memory('$gp + ' + _arg(instr, 1), _arg(instr, 0), 32, false);
@@ -370,6 +415,9 @@ export default {
 		},
 		lhsi: function(instr) {
 			return _load(instr, 16, true);
+		},
+		'lhsi.gp': function(instr) {
+			return Base.read_memory('$gp + ' + _arg(instr, 1), _arg(instr, 0), 16, true);
 		},
 		lbi: function(instr) {
 			return _load(instr, 8, false);
@@ -413,6 +461,22 @@ export default {
 		llw: function(instr) {
 			return _load(instr, 32, false);
 		},
+		/* FLOAT LOADS */
+		fldi: function(instr) {
+			return _load(instr, 64, false);
+		},
+		'fldi.bi': function(instr) {
+			return _load_bi(instr, 64, false);
+		},
+		flsi: function(instr) {
+			return _load(instr, 32, false);
+		},
+		'flsi.bi': function(instr) {
+			return _load_bi(instr, 32, false);
+		},
+		'fld.bi': function(instr) {
+			return _load_bi(instr, 64, false);
+		},
 		/* LOAD POST-INCREMENT */
 		'lwi.bi': function(instr) {
 			return _load_bi(instr, 32, false);
@@ -452,7 +516,7 @@ export default {
 			return _store(instr, 32, false);
 		},
 		'swi37.sp': function(instr) {
-			return _store(instr, 32, false);
+			return Base.write_memory('$sp + ' + _arg(instr, 1), _arg(instr, 0), 32, false);
 		},
 		'swi.gp': function(instr) {
 			return Base.write_memory('$gp + ' + _arg(instr, 1), _arg(instr, 0), 32, false);
@@ -493,6 +557,25 @@ export default {
 		scw: function(instr) {
 			return _store(instr, 32, false);
 		},
+		/* FLOAT STORES */
+		fsd: function(instr) {
+			return _store(instr, 64, false);
+		},
+		fsdi: function(instr) {
+			return _store(instr, 64, false);
+		},
+		'fsdi.bi': function(instr) {
+			return _store_bi(instr, 64, false);
+		},
+		fss: function(instr) {
+			return _store(instr, 32, false);
+		},
+		fssi: function(instr) {
+			return _store(instr, 32, false);
+		},
+		'fssi.bi': function(instr) {
+			return _store_bi(instr, 32, false);
+		},
 		/* STORE POST-INCREMENT */
 		'swi.bi': function(instr) {
 			return _store_bi(instr, 32, false);
@@ -509,6 +592,9 @@ export default {
 		'swi333.bi': function(instr) {
 			return _store_bi(instr, 32, false);
 		},
+		'sw.bi': function(instr) {
+			return _store_bi(instr, 32, false);
+		},
 		/* STACK / MULTI */
 		push25: function(instr) {
 			return Base.write_memory('$sp', _arg(instr, 0), 32, false);
@@ -519,19 +605,73 @@ export default {
 		'lmw.bi': function() {
 			return Base.nop();
 		},
+		'lmw.bim': function() {
+			return Base.nop();
+		},
 		'lmw.adm': function() {
 			return Base.nop();
 		},
 		'smw.bi': function() {
 			return Base.nop();
 		},
+		'smw.bim': function() {
+			return Base.nop();
+		},
 		'smw.adm': function() {
+			return Base.nop();
+		},
+		'smw.bdm': function() {
 			return Base.nop();
 		},
 		lmw: function() {
 			return Base.nop();
 		},
 		smw: function() {
+			return Base.nop();
+		},
+		'lmw?.adm': function() {
+			return Base.nop();
+		},
+		'lmw?.bi': function() {
+			return Base.nop();
+		},
+		'lmwa.aim': function() {
+			return Base.nop();
+		},
+		'lmwa.bd': function() {
+			return Base.nop();
+		},
+		'lmwa.bi': function() {
+			return Base.nop();
+		},
+		'lmwa.bim': function() {
+			return Base.nop();
+		},
+		'lmwzb.am': function() {
+			return Base.nop();
+		},
+		'smw?.ad': function() {
+			return Base.nop();
+		},
+		'smw?.adm': function() {
+			return Base.nop();
+		},
+		'smw?.bdm': function() {
+			return Base.nop();
+		},
+		'smwa.aim': function() {
+			return Base.nop();
+		},
+		'smwzb.a': function() {
+			return Base.nop();
+		},
+		'smwzb.am': function() {
+			return Base.nop();
+		},
+		'smwzb.b': function() {
+			return Base.nop();
+		},
+		'smwzb.bm': function() {
 			return Base.nop();
 		},
 		/* BRANCHES */
@@ -560,19 +700,19 @@ export default {
 			return Base.nop();
 		},
 		beqzs8: function(instr, context) {
-			instr.conditional('$a5', '0', 'EQ');
+			instr.conditional('$ta', '0', 'EQ');
 			return Base.nop();
 		},
 		bnezs8: function(instr, context) {
-			instr.conditional('$a5', '0', 'NE');
+			instr.conditional('$ta', '0', 'NE');
 			return Base.nop();
 		},
 		beqs38: function(instr, context) {
-			instr.conditional(_arg(instr, 0), '$a5', 'EQ');
+			instr.conditional(_arg(instr, 0), '$ta', 'EQ');
 			return Base.nop();
 		},
 		bnes38: function(instr, context) {
-			instr.conditional(_arg(instr, 0), '$a5', 'NE');
+			instr.conditional(_arg(instr, 0), '$ta', 'NE');
 			return Base.nop();
 		},
 		beqc: function(instr, context) {
@@ -609,6 +749,12 @@ export default {
 		jr: function() {
 			return Base.nop();
 		},
+		'jr.dton': function() {
+			return Base.nop();
+		},
+		'jr.iton': function() {
+			return Base.nop();
+		},
 		jr5: function() {
 			return Base.nop();
 		},
@@ -625,6 +771,17 @@ export default {
 				arg = Variable.functionPointer(arg);
 			}
 			return Base.call(arg, []);
+		},
+		'jral.dton': function(instr) {
+			var arg = _arg(instr, 1);
+			if (arg.indexOf('0x') == 0) {
+				arg = Variable.functionPointer(arg);
+			}
+			return Base.call(arg, []);
+		},
+		'jrnez.dton': function(instr) {
+			instr.conditional(_arg(instr, 0), '0', 'NE');
+			return Base.nop();
 		},
 		jral5: function(instr) {
 			var arg = _arg(instr, 0);
@@ -732,6 +889,17 @@ export default {
 			var e = instr.parsed;
 			return Base.assign(e.opd[0], '((1 << ' + e.opd[1] + ') - 1)');
 		},
+		/* FLOAT ARITHMETIC */
+		fmadds: function(instr) {
+			var e = instr.parsed;
+			return Base.assign(e.opd[0], e.opd[1] + ' * ' + e.opd[2] + ' + ' + e.opd[0]);
+		},
+		'fcmpeqd.e': function() {
+			return Base.nop();
+		},
+		fmtsr: function(instr) {
+			return Base.assign(_arg(instr, 0), _arg(instr, 1));
+		},
 		/* SYSTEM / NOP */
 		nop: function() {
 			return Base.nop();
@@ -779,6 +947,39 @@ export default {
 			return Base.call('trap', []);
 		},
 		invalid: function() {
+			return Base.nop();
+		},
+		'invalid?': function() {
+			return Base.nop();
+		},
+		cplwi: function() {
+			return Base.nop();
+		},
+		cpldi: function() {
+			return Base.nop();
+		},
+		cpsdi: function() {
+			return Base.nop();
+		},
+		cpswi: function() {
+			return Base.nop();
+		},
+		'dprefi.d': function() {
+			return Base.nop();
+		},
+		'dprefi.w': function() {
+			return Base.nop();
+		},
+		tlbop: function() {
+			return Base.nop();
+		},
+		misc33_0: function() {
+			return Base.nop();
+		},
+		misc33_1: function() {
+			return Base.nop();
+		},
+		x11b33: function() {
 			return Base.nop();
 		}
 	},
